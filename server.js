f54b234c2b474eb1409e0731d41aa21b13fd8789
Cheezeman24 <reedmarkese12@gmail.com>
2018-02-07 09:48:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var api = require('./api.js');
 var blocked = require('./static/blocked.json');
 var reBlocked = require('./static/re_blocked.json');
 
-var port = process.env.PORT || 80;
+var port = 80 //process.env.PORT || 80;
 var subdomainsAsPath = true;
 var serveHomepage = true;
 var serveHomepageOnAllSubdomains = true;
